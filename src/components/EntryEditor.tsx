603c1bf1e954382fb53f1da3c211d27e53ad6e41
Martin Breest <mbs@spreadshirt.net>
2021-08-06 14:40:46 +0200

    Added simple preview component.

--- src/components/EntryEditor.tsx
+++ src/components/EntryEditor.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
 import { EditorExtensionSDK } from '@contentful/app-sdk';
+import { css } from 'emotion';
 
 interface EditorProps {
   sdk: EditorExtensionSDK;
 }
 
 const Entry = (props: EditorProps) => {
-  return <Paragraph>Hello Entry Editor Component</Paragraph>;
+  const {sdk} = props;  
+  
+  let url:string;
+  if (sdk.contentType.name.startsWith("Component")) {
+    url = `https://spreadshop-contentful-demo.vercel.app/component?id=${sdk.entry.getSys().id}&preview=1&segment=default`;    
+  } else if (sdk.contentType.name.startsWith("PageType")) {        
+    url = `https://spreadshop-contentful-demo.vercel.app/pageType?id=${sdk.entry.getSys().id}&preview=1&segment=default`;    
+  } else {
+    url = "https://spreadshop-contentful-demo.vercel.app/unknown"
+  }
+
+  return (
+    <div>      
+      <iframe src={url} title="preview" className={css({ display: "block", width: "100%", height: "100vh"})}/>;
+    </div>
+  )    
 };
 
 export default Entry;

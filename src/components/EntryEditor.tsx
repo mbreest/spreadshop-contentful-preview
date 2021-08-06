004d9fb1fc68f7c7ab8cc8f839b2e2d3f2488666
Martin Breest <mbs@spreadshirt.net>
2021-08-06 16:48:55 +0200

    Implemented basic preview functionality for page types.

--- src/components/EntryEditor.tsx
+++ src/components/EntryEditor.tsx
@@ -1,5 +1,6 @@
 import { EditorExtensionSDK } from '@contentful/app-sdk';
 import { css } from 'emotion';
+import { isWhiteSpaceSingleLine } from 'typescript';
 
 interface EditorProps {
   sdk: EditorExtensionSDK;
@@ -7,19 +8,125 @@ interface EditorProps {
 
 const Entry = (props: EditorProps) => {
   const {sdk} = props;  
-  
+  const locale = "de";
+    
   let url:string;
   if (sdk.contentType.name.startsWith("Component")) {
     url = `https://spreadshop-contentful-demo.vercel.app/component?id=${sdk.entry.getSys().id}&preview=1&segment=default`;    
-  } else if (sdk.contentType.name.startsWith("PageType")) {        
-    url = `https://spreadshop-contentful-demo.vercel.app/pageType?id=${sdk.entry.getSys().id}&preview=1&segment=default`;    
-  } else {
+  } else if (sdk.contentType.sys.id === "pageLandingpage") { 
+    url = ``;        
+    new Promise((resolve, reject) => {
+      sdk.space.getEntries({
+        limit: 1,
+        include: 10,
+        locale: locale,
+        content_type: 'page',
+        'fields.content.sys.id': sdk.entry.getSys().id,
+        order: 'sys.createdAt',
+      }).then(entries => {
+        const slug = (entries.items[0] as any).fields.slug[locale];
+        document!.getElementById("preview")!.setAttribute("src", `https://spreadshop-contentful-demo.vercel.app/${slug}?preview=1&segment=default`);
+      })      
+    });                      
+  } else if (sdk.contentType.sys.id === "pageProduct") { 
+    url = ``;        
+    new Promise((resolve, reject) => {
+      sdk.space.getEntries({
+        limit: 1,
+        include: 10,
+        locale: locale,
+        content_type: 'page',
+        'fields.content.sys.id': sdk.entry.getSys().id,
+        order: 'sys.createdAt',
+      }).then(entries => {
+        const slug = (entries.items[0] as any).fields.slug[locale];
+        document!.getElementById("preview")!.setAttribute("src", `https://spreadshop-contentful-demo.vercel.app/productrange/detail/${slug}?preview=1&segment=default`);
+      })      
+    });                  
+    
+  } else if (sdk.contentType.sys.id === "pageProduct") { 
+    url = ``;        
+    new Promise((resolve, reject) => {
+      sdk.space.getEntries({
+        limit: 1,
+        include: 10,
+        locale: locale,
+        content_type: 'page',
+        'fields.content.sys.id': sdk.entry.getSys().id,
+        order: 'sys.createdAt',
+      }).then(entries => {
+        const slug = (entries.items[0] as any).fields.slug[locale];
+        document!.getElementById("preview")!.setAttribute("src", `https://spreadshop-contentful-demo.vercel.app/productrange/detail/${slug}?preview=1&segment=default`);
+      })      
+    });                      
+  } else if (sdk.contentType.sys.id === "pageHelpdeskArticle") { 
+    url = ``;        
+    new Promise((resolve, reject) => {
+      sdk.space.getEntries({
+        limit: 1,
+        include: 10,
+        locale: locale,
+        content_type: 'page',
+        'fields.content.sys.id': sdk.entry.getSys().id,
+        order: 'sys.createdAt',
+      }).then(entries => {
+        const slug = (entries.items[0] as any).fields.slug[locale];
+        document!.getElementById("preview")!.setAttribute("src", `https://spreadshop-contentful-demo.vercel.app/helpdesk/${slug}?preview=1&segment=default`);
+      })      
+    });                      
+  } else if (sdk.contentType.sys.id === "pageHelpdeskArticle") { 
+    url = ``;        
+    new Promise((resolve, reject) => {
+      sdk.space.getEntries({
+        limit: 1,
+        include: 10,
+        locale: locale,
+        content_type: 'page',
+        'fields.content.sys.id': sdk.entry.getSys().id,
+        order: 'sys.createdAt',
+      }).then(entries => {
+        const slug = (entries.items[0] as any).fields.slug[locale];
+        document!.getElementById("preview")!.setAttribute("src", `https://spreadshop-contentful-demo.vercel.app/helpdesk/${slug}?preview=1&segment=default`);
+      })      
+    });                      
+  } else if (sdk.contentType.sys.id === "pageBlogPost") { 
+    url = ``;        
+    new Promise((resolve, reject) => {
+      sdk.space.getEntries({
+        limit: 1,
+        include: 10,
+        locale: locale,
+        content_type: 'page',
+        'fields.content.sys.id': sdk.entry.getSys().id,
+        order: 'sys.createdAt',
+      }).then(entries => {
+        const slug = (entries.items[0] as any).fields.slug[locale];
+        document!.getElementById("preview")!.setAttribute("src", `https://spreadshop-contentful-demo.vercel.app/blog/${slug}?preview=1&segment=default`);
+      })      
+    });                      
+  } else if (sdk.contentType.sys.id === "pageBlogCategory") { 
+    url = ``;        
+    new Promise((resolve, reject) => {
+      sdk.space.getEntries({
+        limit: 1,
+        include: 10,
+        locale: locale,
+        content_type: 'page',
+        'fields.content.sys.id': sdk.entry.getSys().id,
+        order: 'sys.createdAt',
+      }).then(entries => {
+        const slug = (entries.items[0] as any).fields.slug[locale];
+        document!.getElementById("preview")!.setAttribute("src", `https://spreadshop-contentful-demo.vercel.app/blog/category/${slug}?preview=1&segment=default`);
+      })      
+    });                      
+  }       
+  else {
     url = "https://spreadshop-contentful-demo.vercel.app/unknown"
   }
 
   return (
     <div>      
-      <iframe src={url} title="preview" className={css({ display: "block", width: "100%", height: "100vh"})}/>;
+      <iframe src={url} id="preview" title="preview" className={css({ display: "block", width: "100%", height: "100vh"})}/>;
     </div>
   )    
 };
